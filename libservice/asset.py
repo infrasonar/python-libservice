c71e9bfda6525fcf85dd52f61a9e0fd4fdf1d0f8
Jeroen van der Heijden <jeroen@cesbit.com>
2023-04-20 10:27:37 +0200

    Upd version

--- libservice/asset.py
+++ libservice/asset.py
@@ -1,4 +1,7 @@
+import base64
 from typing import NamedTuple
+from cryptography.fernet import Fernet
+from .ticonn import ticonn
 
 
 class Asset(NamedTuple):
@@ -9,3 +12,7 @@ class Asset(NamedTuple):
 
     def __repr__(self) -> str:
         return f"asset: {self.asset_id} check: {self.check_id}"
+
+    async def decrypt(self, secret: str):
+        key = await ticonn.run('get_encryption_key', self.asset_id)
+        return Fernet(key).decrypt(base64.b64decode(secret)).decode()
